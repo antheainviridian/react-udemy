cff5700a064c95d0cea18ba46c56bad4402f7215
antheainviridian <94404314+antheainviridian@users.noreply.github.com>
2022-06-28 14:44:12 +1200

    Section 4 End of .53
    Section 4 End of .53

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -34,7 +35,7 @@ return React.createElement(
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
